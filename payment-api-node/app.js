46431d6eced44169fb708b46cf50f5a1730e0e57
huyhung2805 <88467537+huyhung2805@users.noreply.github.com>
2025-09-24 20:43:23 +0700

    Update app.js

--- payment-api-node/app.js
+++ payment-api-node/app.js
@@ -1,12 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");
 const checkoutRoutes = require("./routes/checkout.routes");
 
 const app = express();
-connectDB();
-
 app.use(cors());
 app.use(express.json());
 app.use("/api", checkoutRoutes);
