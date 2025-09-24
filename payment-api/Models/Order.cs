658e77adf9c5119d6db6aabcb320764091eb3691
huyhung2805 <88467537+huyhung2805@users.noreply.github.com>
2025-09-24 19:33:21 +0700

    Update Order.cs

--- payment-api/Models/Order.cs
+++ payment-api/Models/Order.cs
@@ -3,6 +3,7 @@ namespace PaymentApi.Models;
 public class Order
 {
     public int Id { get; set; }
+    public string UserId { get; set; } = string.Empty;
     public string FullName { get; set; } = string.Empty;
     public string Address { get; set; } = string.Empty;
     public string Phone { get; set; } = string.Empty;
